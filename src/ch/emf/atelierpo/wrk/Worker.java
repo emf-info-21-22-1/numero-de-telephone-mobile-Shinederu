9b9a0e054cda9daf4ade97591c1d661f1f379b46
shine <shine@RogFlowX16>
2023-03-28 09:27:36 +0200

    JavaDoc V1 controleNumeroTelMobile

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -8,6 +8,16 @@ package ch.emf.atelierpo.wrk;
 public class Worker {
 
 
+    /**
+     * 
+     * Cette méthode permets de vérifier si le numéro founir est valide, pour celà seul 4 préfix sont authorisés: "+4176", "+4177", "+4178" et "+4179"
+     * Les caractère authorisé pour le numéro sont les chiffres (0-9) et les espaces.
+     * 
+     * 
+     * @param prefix indique la base du numéro en suivant les standards authorisés:
+     * @param numero indique la suite du numéro (0-9 et espace uniquement)
+     * @return Retourne true si le numéro est valide, sinon false
+     */
     public boolean controleNumeroTelMobile(String prefix, String numero) {
         return false;
     }
